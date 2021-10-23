6a6b9fba31fbefad2b57598278a02e79b14efe65
aizatazhar <aizat.azharr@gmail.com>
2021-10-23 15:15:43 +0800

    Implement add_employee

--- proc.sql
+++
@@ -1,5 +0,0 @@
-CREATE OR REPLACE FUNCTION add_department(did INTEGER, dname TEXT) RETURNS VOID AS $$
-    BEGIN
-        INSERT INTO Departments(did, dname) VALUES (did, dname);
-    END;
-$$ LANGUAGE plpgsql;
--- schema.sql
+++ schema.sql
@@ -1,23 +1,28 @@
 DROP TABLE IF EXISTS Employees, Contact_Numbers, Junior, Booker, Senior, Manager, Departments, Meeting_Rooms, 
     Health_Declaration, Updates, Sessions, Joins CASCADE;
 
+DROP TYPE IF EXISTS KIND CASCADE;
+
 SET timezone = 'America/Los_Angeles';
 
-CREATE TABLE Departments (
-    did INTEGER,
-    dname TEXT,
+-- ##################
+--       Tables
+-- ##################
 
-    PRIMARY KEY (did)
+CREATE TYPE KIND AS ENUM ('Junior', 'Senior', 'Manager');
+
+CREATE TABLE Departments (
+    did SERIAL PRIMARY KEY,
+    dname TEXT
 ); 
 
 CREATE TABLE Employees (
-    eid INTEGER,
+    eid SERIAL PRIMARY KEY,
     ename TEXT,
     email TEXT,
     did INTEGER,
     resigned_date DATE,
 
-    PRIMARY KEY (eid),
     FOREIGN KEY (did) REFERENCES Departments
 );
 
@@ -114,5 +119,68 @@ CREATE TABLE Joins (
     FOREIGN KEY (eid) REFERENCES Employees
 ); 
 
+-- ###########################
+--        Basic Functions
+-- ###########################
+
+CREATE OR REPLACE FUNCTION add_department(dname TEXT) RETURNS VOID AS $$
+    BEGIN
+        INSERT INTO Departments (dname) VALUES (dname);
+    END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION add_employee(ename TEXT, contact_number TEXT, kind KIND, department_name TEXT) RETURNS VOID AS $$
+    DECLARE
+        created_eid INTEGER;
+        created_email TEXT;
+        matching_did INTEGER = NULL;
+    BEGIN
+        -- Find the did based on given department name
+        SELECT did FROM Departments WHERE dname = department_name INTO matching_did;
+        IF matching_did IS NULL THEN 
+            RAISE EXCEPTION 'No such department with the name %', department_name;
+        END IF;
+
+        -- Temporarily set email to be NULL as we require the auto-generated eid to create email
+        INSERT INTO Employees(ename, email, did, resigned_date) 
+        VALUES (ename, NULL, matching_did, NULL) 
+        RETURNING eid INTO created_eid;
+
+        -- Create and set email by concatenating name and eid (guaranteed to be unique)
+        created_email = CONCAT(ename, created_eid::TEXT, '@company.com');
+        UPDATE Employees SET email = created_email WHERE eid = created_eid;
+
+        -- Insert into respective subtable based on kind
+        -- TODO: Do we need to insert into Booker as well?
+        CASE 
+            WHEN kind = 'Junior' THEN
+                INSERT INTO Junior VALUES (created_eid);
+            WHEN kind = 'Senior' THEN
+                INSERT INTO Senior VALUES (created_eid);
+            WHEN kind = 'Manager' THEN
+                INSERT INTO Manager VALUES (created_eid);
+        END CASE;
+    END;
+$$ LANGUAGE plpgsql;
+
+-- #############################
+--         Core Functions
+-- #############################
+
+CREATE OR REPLACE FUNCTION add_department(dname TEXT) RETURNS VOID AS $$
+    BEGIN
+        INSERT INTO Departments (dname) VALUES (dname);
+    END;
+$$ LANGUAGE plpgsql;
+
+-- #############################
+--        Health Functions
+-- #############################
 
 
+
+
+
+-- #############################
+--        Admin Functions
+-- #############################
